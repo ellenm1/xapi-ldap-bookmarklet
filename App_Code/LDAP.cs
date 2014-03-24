d0d72fed6b6754f34b9143315445e3432770af55
Ellen Meiselman <ellenm1@gmail.com>
2014-03-24 18:15:55 -0400

    first commit

---
+++ App_Code/LDAP.cs
@@ -0,0 +1,197 @@
+﻿using System;
+using System.Net;
+
+using System.DirectoryServices;
+using System.DirectoryServices.Protocols;
+using System.Security.Permissions;
+using System.DirectoryServices.AccountManagement;
+
+namespace ConnectLDAP
+{
+    //[DirectoryServicesPermission(SecurityAction.LinkDemand, Unrestricted = true)]
+
+
+    public class LDAPConnect
+    {
+        #region Fields
+        // static variables used throughout the example
+        static LdapConnection ldapConnection;
+        static string ldapServer;
+        static NetworkCredential credential;
+        static string targetOU; // dn of an OU. eg: "OU=sample,DC=fabrikam,DC=com"
+        static string response;
+        static bool result;
+  
+
+        #endregion
+
+        #region Properties
+        public bool Result
+        {
+            get
+            {
+                return result;
+            }
+        }
+ 
+        public string Response
+        {
+
+            get
+            {
+                return response;
+            }
+        }
+        #endregion
+
+        # region Methods
+        //GetParameters property setter
+        static void setParameters(string ldapHost, string ldapUser, string ldapPwd, string ldapDomain, string ldapTarget)
+        {
+            // When running: ConnectLDAP.exe <ldapServer> <user> <pwd> <domain> <targetOU>
+
+            // if (args.Length != 5)
+            // {
+            //     Console.WriteLine("Usage: ConnectLDAP.exe <ldapServer> <user> <pwd> <domain> <targetOU>");
+            //    Environment.Exit(-1);// return an error code of -1
+            // }
+
+            // test arguments to insure they are valid and secure
+
+            // initialize variables
+            ldapServer = ldapHost;
+            credential = new NetworkCredential(ldapUser, ldapPwd);
+            targetOU = ldapTarget;
+        }
+
+     
+        #endregion
+
+        #region Constructor
+        /// <summary>
+        /// Creates LDAP connection
+        /// </summary>
+        /// <param name="ldapServer">LDAP server</param>
+        /// <param name="user">LDAP user login</param>
+        /// <param name="password">LDAP user password</param>
+        /// <param name="targetOU">LDAP target OU</param>
+
+        public LDAPConnect(string ldapHost, string ldapUser, string ldapPwd, string ldapDomain, string ldapTarget)
+        {
+            setParameters(ldapHost, ldapUser, ldapPwd, ldapDomain, ldapTarget);  // Set the properties
+
+            //Create LDAP Entry point
+            DirectoryEntry deService = new DirectoryEntry();
+
+
+
+
+
+            deService.Path = "LDAP://" + ldapHost + "/" + ldapTarget;
+            deService.Username = ldapUser;
+            //deService .Password = ldapPwd;
+            deService.AuthenticationType = AuthenticationTypes.Anonymous;
+            //Bind to the native AdsObject to force authentication.			
+            Object obj = deService.NativeObject;
+
+            DirectorySearcher dsSearch = new DirectorySearcher(deService);
+            dsSearch.Filter = "(cn=" + ldapUser + ")";
+            dsSearch.PropertiesToLoad.Add("uid");
+            
+            try
+            {
+                SearchResult srResult = dsSearch.FindOne();
+                
+                if (srResult != null)
+                {
+                    if (srResult.Properties.Contains("lockoutTime"))
+                    {
+                        if (Int64.Parse(srResult.Properties["lockoutTime"][0].ToString()) != 0)
+                        {
+                            //they are locked out... so throw error or return false;
+                            result = false;
+                            response = "User is locked out";
+                        }
+                    }
+                    //otherwise, verify creds.
+                    DirectoryEntry deUser = srResult.GetDirectoryEntry();
+                    deUser.Username = ldapUser;
+                    deUser.Password = ldapPwd;
+                    deUser.AuthenticationType = AuthenticationTypes.Secure;
+                    try
+                    {
+                        //just create a var, it will fail if creds are wrong.
+                        string path = deUser.Path;
+                        result = false;
+                        response = "Good credentials"; // ok, good creds if you got here
+                    }
+                    catch (Exception ex)
+                    {
+                        //so, this means their password was bad... NOT the username.
+                        result = false;
+                        response = "Bad credentials";
+                    }
+                    finally
+                    {
+                        deUser.Dispose();
+                    }
+                }
+                else
+                {
+                    //throw an error here since you did not find the user (bad username)
+                    result = false;
+                    response = "Did not find user";
+                }
+            }
+            catch (Exception e)
+            {
+                result = false;
+                response = "Authentication server is offline or VPN not established.";
+            }
+            finally
+            {
+                deService.Dispose();
+                dsSearch.Dispose();
+            }
+        }
+
+        public LDAPConnect(string ldapHost, string ldapuser, string ldapPwd,string ldapTarget)
+        {
+            // create a "principal context" - e.g. your domain (could be machine, too)
+            using(PrincipalContext pc = new PrincipalContext(ContextType.ApplicationDirectory, ldapHost, ldapTarget))
+            {
+                // validate the credentials
+              
+                result = pc.ValidateCredentials("CN=" + ldapuser , ldapPwd);
+
+            }
+            //return result;
+
+
+        }
+         #endregion
+
+
+
+        public LDAPConnect(string ldapHost,string ldapUser, string ldapPwd)
+        {
+            try
+            {
+                setParameters(ldapHost, ldapUser, ldapPwd, "umich.edu", "ou=people,dc=med,dc=umich,dc=edu");  // Set the properties
+
+                // Create the new LDAP connection
+                ldapConnection = new LdapConnection(ldapServer);
+                ldapConnection.Bind(credential);
+                //ldapConnection.Credential = credential;
+                //Console.WriteLine("LdapConnection is created successfully.");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("\r\nUnexpected exception occured:\r\n\t" + e.GetType() + ":" + e.Message);
+            }
+        }
+
+
+
+    }
+}
---
+++ App_Code/UMICHLdap.cs
@@ -0,0 +1,240 @@
+﻿//
+// Don't forget to remove the HTML tags above and at the end of the file
+using System;
+using System.Collections;
+using System.Xml;
+using System.DirectoryServices;
+using System.Runtime.InteropServices;
+using System.DirectoryServices.Protocols;
+using System.Web;
+using System.IO;
+
+
+
+namespace ConnectUmichLDAP
+{
+
+    /// 
+    /// Example of LDAP Authentication code using ADSI with MCIT custom error messages
+    /// written by Dmitriy Kashchenko, IDM team
+    public class LdapAuthentication
+    {
+        # region fields
+        // const
+        private const string ERR_NOT_FOUND = "(0x80072030): not found";
+
+        // LDAP metrics
+        private string _ldapserver = "LDAP://ldap.ent.med.umich.edu:636/";
+        private string _topContainer = "ou=people,dc=med,dc=umich,dc=edu";
+        private String _defaultFilter = "(uid={0})";
+
+        // errors handling
+        // messages file name
+        private string _messages = "ldapautherrors.xml";
+        // variables
+        private string _errorMsg;
+        private string _provider;
+        private const string nodeKey = "message";
+        private const string indexAttr = "index";
+        private Hashtable errors = null;
+        #endregion
+
+        # region methods
+        private void parseFile(string fname)
+        {
+            string virtualPath = "HTTP://"  + HttpContext.Current.Request.Url.Authority  + HttpContext.Current.Request.ApplicationPath + "/" + fname;
+            XmlTextReader node = new XmlTextReader(virtualPath);
+            node.WhitespaceHandling = WhitespaceHandling.None;
+
+            while (node.Read())
+            {
+                if (node.NodeType == XmlNodeType.Element && node.Name.Equals(nodeKey))
+                {
+                    if (node.HasAttributes)
+                    {
+                        string index = node.GetAttribute("index");
+                        node.Read();
+                        string message = node.Value;
+                        errors.Add(index, message);
+                    }
+                }
+            }
+
+            node.Close();
+        }
+
+        [DllImport("activeds.dll", ExactSpelling = true, EntryPoint = "ADsGetLastError", CharSet = System.Runtime.InteropServices.CharSet.Unicode)]
+        private static extern int ADsGetLastError(ref int error, IntPtr errorbuf, int errorbuflen, IntPtr namebuf, int namebuflen);
+
+        private int getExtendedError()
+        {
+            IntPtr errorbuf = (IntPtr)0;
+            IntPtr namebuf = (IntPtr)0;
+            int error = 0;
+
+            try
+            {
+                errorbuf = Marshal.AllocHGlobal(256 * 2);
+                namebuf = Marshal.AllocHGlobal(256 * 2);
+                ADsGetLastError(ref error, errorbuf, 256, namebuf, 256);
+                _errorMsg = Marshal.PtrToStringUni(errorbuf);
+                _provider = Marshal.PtrToStringUni(namebuf);
+
+                return error;
+            }
+            finally
+            {
+                if (errorbuf != (IntPtr)0) Marshal.FreeHGlobal(errorbuf);
+                if (namebuf != (IntPtr)0) Marshal.FreeHGlobal(namebuf);
+            }
+        }
+
+        public string getErrorMessage(Exception e)
+        {
+            string rc = e.ToString();
+            if (errors != null)
+            {
+                int ee = getExtendedError();
+                if (ee != 0) rc = _errorMsg;
+                foreach (DictionaryEntry entry in errors)
+                {
+                    string index = (string)entry.Key;
+                    if (rc.IndexOf(index) != -1)
+                    {
+                        rc = (string)entry.Value;
+                        break;
+                    }
+                }
+            }
+
+            return  rc;
+        }
+
+        #endregion
+
+        #region constructor
+        /// <summary>
+        /// Creates new UMICH LDAP array with the user's email address and userName
+        /// </summary>
+        public LdapAuthentication()
+        {
+            errors = new Hashtable();
+            parseFile(_messages);
+        }
+        #endregion
+
+        /// <summary>
+        /// Old authentication method, don't use
+        /// </summary>
+        /// <param name="user"></param>
+        /// <param name="password"></param>
+        /// <returns></returns>
+        public string[] AuthenticateOld(string user, string password)
+        {
+            string[] results = new string[2];
+
+
+            //Concatenate serverpath + username + container
+            //I.e.  "LDAP://ldap.disney.com:636/CN=donaldDuck,ou=people,dc=la,dc=disney,dc=com"
+            DirectoryEntry de = new DirectoryEntry(_ldapserver + "cn=" + user + "," + _topContainer);
+            //+ "cn=" + user + ","
+            de.Username = "cn=" + user + "," + _topContainer;
+
+
+            //User's password for initial verification
+            de.Password = password;
+
+            //initate anonymous bind
+            de.AuthenticationType = System.DirectoryServices.AuthenticationTypes.SecureSocketsLayer;
+            DirectorySearcher searcher = new DirectorySearcher(de);
+            searcher.PropertiesToLoad.Add("sn");
+            searcher.PropertiesToLoad.Add("mail");
+            searcher.PropertiesToLoad.Add("givenName");
+            //Search for first record
+            SearchResult result = searcher.FindOne();
+
+            //Check results
+            if (result == null) throw new Exception(ERR_NOT_FOUND);
+
+            de = result.GetDirectoryEntry();
+            //Validate password, this will trigger exception if user not found
+            Object obj1 = de.NativeObject;
+
+            //Return search results
+            results[0] = (string)de.Properties["mail"].Value;
+            results[1] = (string)de.Properties["givenName"].Value + " " + (string)de.Properties["sn"].Value;
+            // Distingushed Name of the found account
+            //string DN = de.Path.Substring(de.Path.ToUpper().IndexOf("CN="));
+            // Close search connection
+            searcher.Dispose();
+            de.Close();
+
+            //if we made it here, we successfully authenticated
+            return results;
+        }
+
+       /// <summary>
+       /// Authenticates user against UMICH LDap server and returns array[2] with user email and full name
+       /// </summary>
+       /// <param name="user"></param>
+       /// <param name="password"></param>
+       /// <returns></returns>
+ 
+        public string[] AuthenticateLDAP(string user, string password)
+        {
+            string[] results = new string[2];
+
+            DirectoryEntry de = new DirectoryEntry(_ldapserver + _topContainer);
+            // required to initate anonymous bind
+            de.AuthenticationType = System.DirectoryServices.AuthenticationTypes.ServerBind;
+            DirectorySearcher searcher = new DirectorySearcher(de);
+
+            //set the filter to search UID first. 
+            //the filter syntax is the standard LDAP filter syntax
+            //Cannot authenticate against UID/pwd combo, only CN/pwd combo
+            //So we search on UID, to find CN
+            searcher.Filter = string.Format(_defaultFilter, user);
+            SearchResult result = searcher.FindOne();
+            if (result == null) throw new Exception(ERR_NOT_FOUND);
+
+            de = result.GetDirectoryEntry();
+            // Distingushed Name of the found account, 
+            // extract CN for authentication
+            string DN = de.Path.Substring(de.Path.ToUpper().IndexOf("CN="));
+
+            // Close search connection and free resources up
+            de.Close();
+            searcher.Dispose();
+
+            // now bind with CN and authenticate user
+            //Concatenate serverpath + username + container
+            //I.e.  "LDAP://ldap.disney.com:636/CN=donaldDuck,ou=people,dc=la,dc=disney,dc=com"
+            de = new DirectoryEntry(_ldapserver + "cn=" + user + "," + _topContainer);
+            de.Username = DN;
+            de.Password = password;
+            de.AuthenticationType = System.DirectoryServices.AuthenticationTypes.SecureSocketsLayer;
+            Object obj = de.NativeObject;
+
+            //We need to re initiate DirectorySearcher so that we can specify/limit the properties loaded
+            searcher = new DirectorySearcher(de);
+            searcher.PropertiesToLoad.Add("sn");
+            searcher.PropertiesToLoad.Add("mail");
+            searcher.PropertiesToLoad.Add("givenName");
+
+            //Now Search again for first record found
+            SearchResult result2 = searcher.FindOne();
+            de = result2.GetDirectoryEntry();
+
+            //Store search results for return
+            results[0] = (string)de.Properties["mail"].Value;
+            results[1] = (string)de.Properties["givenName"].Value + " " + (string)de.Properties["sn"].Value;
+
+            //Clean up resources
+            de.Close();
+            searcher.Dispose();
+
+            //return string array
+            return results;
+        }
+    }
+}
